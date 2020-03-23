e6b4a1ffe7cedaa4e3eb38a272897f2d3c0a3926
Rachael <rj04maha@siena.edu>
2020-03-23 12:05:53 -0400

    morning update 3.23

--- client/src/reducers/photosReducers.js
+++ client/src/reducers/photosReducers.js
@@ -4,8 +4,8 @@ export default (state = {}, action) => {
   switch (action.type) {
     case "FETCH_PHOTOS":
       return { ...state, ..._.mapKeys(action.payload, "_id") };
-    case "ADD_PHOTO":
-      return { ...state, [action.payload._id]: action.payload };
+    //case "ADD_PHOTO":
+    //return { ...state, [action.payload._id]: action.payload };
     case "FETCH_PHOTO":
       return { ...state, [action.payload._id]: action.payload };
     case "UPDATE_PHOTO":
--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,9 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   googleUserName: process.env.GOOGLE_USERNAME,
   googlePass: process.env.GOOGLE_PASSWORD,
-  adminPassword: process.env.ADMIN_PASSWORD
+  adminPassword: process.env.ADMIN_PASSWORD,
+  secret: process.env.SECRET,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRETS
 };
